c7ef1ef8fd8a466c513f5421eae604d449824296
CJ <90126284+CeeJank@users.noreply.github.com>
2025-03-01 19:21:18 +0800

    initial commit

---
+++ lib/main.rb
@@ -0,0 +1,11 @@
+
+
+puts "Welcome to tic-tac-toe!"
+print "Please input the name of Player1: "
+p1 = gets.chomp
+print "Please input the name of Player2: "
+p2 = gets.chomp
+
+puts "#{p1} will be O, #{p2} will be X, choose your option based on the grid numbers"
+puts "#{p1} vs #{p2}, game start!"
+
